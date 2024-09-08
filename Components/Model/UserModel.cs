32762780c2dfda4c32fcc0f01bafad3465fe4b23
fitristachan <120694803+fitristachan@users.noreply.github.com>
2024-09-08 22:00:31 +0700

    Add project files.

---
+++ Components/Model/UserModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DataUser.Components.Model
+{
+    public class DataDiri
+    {
+        public string? Nama { get; set; }
+        public string? NoHp { get; set; }
+        public string? Alamat { get; set; }
+    };
+}
